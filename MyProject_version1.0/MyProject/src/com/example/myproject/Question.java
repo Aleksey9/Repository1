cc764035be9e08a9f0cae3342ce00321f087a9c8
Aleksey9 <aleksey599@mail.ru>
2016-03-29 18:35:22 +0300

    Question

--- MyProject_version1.0/MyProject/src/com/example/myproject/Question.java
+++ MyProject_version1.0/MyProject/src/com/example/myproject/Question.java
@@ -63,6 +63,44 @@ public class Question {
 	}
 	
 	public Question(String strQuestion) {
+		//создаём список, в который будем заносить ответы на вопрос:
+		ArrayList <String> qList = new ArrayList();
+		//Обрабатываем вопросную строку:
+		String str = strQuestion;
+		int p = str.indexOf("_");
+		//Заносим название вопроса в сам объект:
+		this.name = str.substring(0, p);
+		//Заносим по очереди ответы на вопрос в список: 
+		for (int i = 0; i < this.N; ++i) {
+			qList.add(str.substring(p + 1, str.indexOf("_", p + 1)));
+			p = str.indexOf("_", p + 1);
+		}
+		//Запоминаем номер правильного ответа:
+		Integer cA = new Integer(str.substring(p + 1));
+		int corAns = (int) (cA);
+		//Теперь будем случайным образом вытаскивать из списка ответы и заносить их в массив:
+		//Индекс случайного элемента списка:
+		int rand;
+		//Индекс массива объекта:
+		int i = 0;
+		//Вытаскиваем из списка элементы, пока он не кончится:
+		while (qList.size() > 0) {
+			//Генерируем случайный индекс:
+			rand = (int) (qList.size()*Math.random());
+			//Записываем этот случайный элемент в массив объекта:
+			this.ans[i] = qList.get(rand);
+			//Удаляем этот элемент:
+			qList.remove(rand);
+			//Если индекс случайного элемента меньше индекса правильного ответа, то:
+			if (rand < corAns) {
+				corAns -= 1; // уменьшаем индекс правильного ответа на единицу
+			} else if (rand == corAns) { // а если индекс случайного элемента равен индексу правильного ответа, то:
+				this.correctAnswer = i; //говорим, что индекс правильного ответа - это i
+			}
+			++i; //идём дальше
+		}
+		return;
+		/*
 		String str = strQuestion;
 		int p = str.indexOf("_");
 		this.name = str.substring(0, p);
@@ -73,6 +111,7 @@ public class Question {
 		Integer cA = new Integer(str.substring(p + 1));
 		this.correctAnswer = (int) (cA);
 		return;
+		*/
 	}
 	
 	
